47778ccc569b8ce766388d80d97143f8332b3fca
Karel Parkkola <96303161+Ben-PP@users.noreply.github.com>
2024-08-16 19:25:50 +0300

    Added external tickets (#4)

--- sanity.cli.js
+++ sanity.cli.js
@@ -1,8 +1,17 @@
-import { defineCliConfig } from "sanity/cli";
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: "ubo8m1s0",
-    dataset: "production",
+    projectId: 'ubo8m1s0',
+    dataset: 'production'
   },
-});
+  graphql: [
+    {
+      projectId: 'ubo8m1s0',
+      tag: 'website',
+      dataset: 'production',
+      workspace: 'dumppi-website',
+      id: 'website'
+    }
+  ]
+})
